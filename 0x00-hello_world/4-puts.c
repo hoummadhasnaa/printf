7f68752c71c33e8aa3fed83521f90e09d6000ae5
hasnaa <hoummad600@gmail.com>
2023-09-07 14:11:42 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -this prints hello, puts
+ * main - this prints hello, puts
+ *
  * return: 0 (Successs)
  */
-
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }	
