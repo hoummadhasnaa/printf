2c0dd4e21490877db534159601347e411c745eac
hasnaa <hoummad600@gmail.com>
2023-09-07 15:52:34 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,11 @@
 /**
  * main - this is print for the function printf
  *
+ * Description: 'printf'
+ *
  * Return Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
