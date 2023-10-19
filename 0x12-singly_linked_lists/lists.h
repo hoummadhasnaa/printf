ed60b47a3b44869cbd5e9d45724ef393b79d1bfb
hasnaa <hoummad600@gmail.com>
2023-10-19 15:41:06 -0700

    stingly

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,20 @@
 #ifndef lists_h
 #define lists_h
 
-#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - stringly linked list
+ */
+
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
@@ -10,4 +23,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 
-#endif
+#endif /*linked*/
